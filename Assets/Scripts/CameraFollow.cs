cec699269d7236bcfb665748d41463fc08b264e7
noxayyyy <noxayyyy@users.noreply.github.com>
2022-10-29 17:13:34 +0500

    Initial Commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	Transform Player;
+	Vector3 TempPos;
+	[SerializeField]
+	float MinX, MaxX;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Player = GameObject.FindWithTag("Player").transform;
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		TempPos = transform.position;
+		TempPos.x = Player.position.x;
+		if(TempPos.x > MaxX)
+		{
+			TempPos.x = MaxX;
+		}
+		else if (TempPos.x < MinX)
+		{
+			TempPos.x = MinX;
+		}
+		transform.position = TempPos;
+		
+	}
+}
---
+++ Assets/Scripts/FancyMan.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancyMan : MonoBehaviour
+{
+	[SerializeField]
+	float MoveF = 10f, JumpF = 11f;
+	string RUN_ANIM = "Run", JUMP_ANIM = "Jump", GROUND_TAG = "Ground";
+	float MoveX;
+	bool isGrounded;
+
+	SpriteRenderer Render;
+	[SerializeField]
+	Rigidbody2D Body;
+	Animator Anim;
+
+	private void Awake()
+	{
+		Body = GetComponent<Rigidbody2D>();
+		Anim = GetComponent<Animator>();
+		Render = GetComponent<SpriteRenderer>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		PlayerMoveKeyboard();
+		AnimatePlayer();
+		PlayerJump();
+	}
+
+	void PlayerMoveKeyboard()
+	{
+		MoveX = Input.GetAxis("Horizontal");
+		transform.position += new Vector3(MoveX, 0f, 0f) * Time.deltaTime * MoveF;
+
+	}
+
+	void AnimatePlayer()
+	{
+		if(MoveX > 0) // move right
+		{
+			Anim.SetBool(RUN_ANIM, true);
+			Render.flipX = false;
+		}
+		else if(MoveX < 0) // move left
+		{
+			Anim.SetBool(RUN_ANIM, true);
+			Render.flipX = true;
+		}
+		else
+		{
+			Anim.SetBool(RUN_ANIM, false);
+		}
+	}
+
+	void PlayerJump()
+	{
+		if(Input.GetButtonDown("Jump") && isGrounded)
+		{
+			isGrounded = false;
+			Body.AddForce(new Vector2(0f, JumpF), ForceMode2D.Impulse);
+			Anim.SetBool(JUMP_ANIM, true);
+		}	
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.CompareTag(GROUND_TAG))
+		{
+			isGrounded = true;
+			Anim.SetBool(JUMP_ANIM, false);
+		}
+	}
+}//class
